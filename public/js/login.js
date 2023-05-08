574be75cddb40e6e698cc20fc490b793f61d059b
jonnyboy808 <125600257+jonnyboy808@users.noreply.github.com>
2023-05-08 13:40:38 -0700

    added signup js

--- public/js/login.js
+++ public/js/login.js
@@ -23,3 +23,31 @@ const loginFormHandler = async function(event) {
   document
     .querySelector('#login-form')
     .addEventListener('submit', loginFormHandler);
+
+    const signupFormHandler = async function(event) {
+        event.preventDefault();
+      
+        const signupUserEl = document.querySelector('#username-signup');
+        const signupEmailEl = document.querySelector('#email-signup')
+        const signupPassEl = document.querySelector('#password-input-signup');
+      
+        const response = await fetch('/api/user', {
+          method: 'POST',
+          body: JSON.stringify({
+            username: signupUserEl.value,
+            email: signupEmailEl.value,
+            password: signupPassEl.value,
+          }),
+          headers: { 'Content-Type': 'application/json' },
+        });
+      
+        if (response.ok) {
+          document.location.replace('/dashboard');
+        } else {
+          alert('Failed to sign up');
+        }
+      };
+      
+      document
+        .querySelector('#signup-form')
+        .addEventListener('submit', signupFormHandler);
