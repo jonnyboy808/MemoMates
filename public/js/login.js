92b60f1833ef4882941234abdee5114a045feb1a
jonnyboy808 <125600257+jonnyboy808@users.noreply.github.com>
2023-05-08 13:45:40 -0700

    configured with main handlebars

--- public/js/login.js
+++ public/js/login.js
@@ -14,7 +14,7 @@ const loginFormHandler = async function(event) {
     });
   
     if (response.ok) {
-      document.location.replace('/dashboard');
+      document.location.replace('/main');
     } else {
       alert('Failed to login');
     }
@@ -42,7 +42,7 @@ const loginFormHandler = async function(event) {
         });
       
         if (response.ok) {
-          document.location.replace('/dashboard');
+          document.location.replace('/main');
         } else {
           alert('Failed to sign up');
         }
