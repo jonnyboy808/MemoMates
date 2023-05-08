167fcfcd8e14e339d139c7866d2ca6b6beef9862
jonnyboy808 <125600257+jonnyboy808@users.noreply.github.com>
2023-05-08 13:28:08 -0700

    created login js file

---
+++ public/js/login.js
@@ -0,0 +1,25 @@
+const loginFormHandler = async function(event) {
+    event.preventDefault();
+  
+    const emailEl = document.querySelector('#email-login');
+    const passwordEl = document.querySelector('#password-login');
+  
+    const response = await fetch('/api/user/login', {
+      method: 'POST',
+      body: JSON.stringify({
+        username: emailEl.value,
+        password: passwordEl.value,
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to login');
+    }
+  };
+  
+  document
+    .querySelector('#login-form')
+    .addEventListener('submit', loginFormHandler);
