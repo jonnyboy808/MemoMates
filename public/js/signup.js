d6f2b8ab19c3ab91bf286f085aa65f7b7a38a7c7
jonnyboy808 <125600257+jonnyboy808@users.noreply.github.com>
2023-05-10 11:40:00 -0700

    made changes to sign up

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,24 +1,28 @@
-const signupFormHandler = async function(event) {
+const signupFormHandler = async (event) => {
     event.preventDefault();
   
     // queryselector for username and password
-    const email = document.querySelector('#email-input-signup');
-    const password = document.querySelector('#password-input-signup');
+    const emailEl = document.querySelector("#email-input-signup").value;
+    const passwordEl = document.querySelector("#password-input-signup").value;
   
-    const response = await fetch('/api/users', {
-      method: 'POST',
-      body: JSON.stringify({ email, password }),
-      headers: { 'Content-Type': 'application/json' },
+
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({ 
+        email: emailEl.value,
+        password: passwordEl.value,
+}),
+      headers: { "Content-Type": "application/json" },
     });
   
     if (response.ok) {
-      document.location.replace('/connection');
+      document.location.replace("/connection");
     } else {
-      alert('Failed to sign up');
+      alert("Failed to sign up");
     }
   };
   
   document
-    .querySelector('#signup-form')
-    .addEventListener('submit', signupFormHandler);
+    .querySelector("#signup-form")
+    .addEventListener("submit", signupFormHandler);
   
