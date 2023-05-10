15d95950e16384dc892fe56c65574e58ab611f8b
Beenz <CH.SABEEN@GMAIL.COM>
2023-05-09 21:45:57 -0700

    get route added for rendering new connection form

--- public/js/connection.js
+++ public/js/connection.js
@@ -1,3 +1,29 @@
+const newFormHandler = async (event) => {
+  event.preventDefault();
+
+  const name = document.querySelector("#connection-name").value.trim();
+  const history = document.querySelector("#history").value.trim();
+
+  if (name && history) {
+    const response = await fetch("/api/notes", {
+      method: "POST",
+      body: JSON.stringify({ name, history }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    console.log("newFormHandler: api/post POST method called");
+
+    if (response.ok) {
+      console.log("newFormHandler: response was OK");
+      document.location.replace("/connection");
+    } else {
+      alert("Failed to create post");
+    }
+  }
+};
+
 // const delButtonHandler = async (event) => {
 //   if (event.target.hasAttribute("data-id")) {
 //     const id = event.target.getAttribute("data-id");
