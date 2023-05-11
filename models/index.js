8c78a4a2c9f772cab8d8f803aa122858eee35d3d
Beenz <CH.SABEEN@GMAIL.COM>
2023-05-11 10:19:17 -0700

    deleted extra files

--- models/index.js
+++ models/index.js
@@ -1,6 +1,5 @@
 const User = require("./User");
 const Note = require("./Note");
-const Items = require("./Items");
 
 User.hasMany(Note, {
   foreignKey: "user_id",
@@ -11,8 +10,4 @@ Note.belongsTo(User, {
   foreignKey: "user_id",
 });
 
-Note.hasMany(Items, {});
-
-Items.belongsTo(Note, {});
-
-module.exports = { User, Note, Items };
+module.exports = { User, Note };
